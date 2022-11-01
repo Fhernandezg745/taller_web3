baef6f725315c063ae13f7b94460f3e36fcc6c69
Fernando H <fer.hndez@gmail.com>
2022-11-01 13:28:47 -0300

    contrato erc20 gold y exp

---
+++ Experience.sol
@@ -0,0 +1,67 @@
+// SPDX-License-Identifier: GPL-3.0
+
+pragma solidity >=0.7.0 <0.9.0;
+
+contract Experience {
+    //funciones getter
+    string public name;
+    string public symbol;
+    uint8 public decimals;
+    uint256 public totalSupply;
+    mapping(address => uint256) public balanceOf;
+    mapping(address => mapping(address => uint256)) public allowance;
+
+    constructor() public {
+        name = "Experience";
+        symbol = "EXP";
+        decimals = 18;
+        totalSupply = 1000 * (uint256(10)**decimals);
+        balanceOf[msg.sender] = totalSupply;
+    }
+
+    //eventos
+    event Transfer(address indexed _from, address indexed _to, uint256 _value);
+    event Approval(
+        address indexed _owner,
+        address indexed _spender,
+        uint256 _value
+    );
+
+    //funciones setter
+
+    //transfer, primero require de que el balance del sender sea mayor o igual al valor que esta enviando
+    //Compensar el valor de transferencia
+    function transfer(address _to, uint256 _value)
+        public
+        returns (bool success)
+    {
+        require(balanceOf[msg.sender] >= _value);
+        balanceOf[msg.sender] -= _value;
+        balanceOf[_to] += _value;
+        emit Transfer(msg.sender, _to, _value);
+        return true;
+    }
+
+    function transferFrom(
+        address _from,
+        address _to,
+        uint256 _value
+    ) public returns (bool success) {
+        require(balanceOf[_from] >= _value);
+        require(allowance[_from][msg.sender] >= _value);
+        balanceOf[_from] -= _value;
+        balanceOf[_to] += _value;
+        allowance[_from][msg.sender] -= _value;
+        emit Transfer(_from, _to, _value);
+        return true;
+    }
+
+    function approve(address _spender, uint256 _value)
+        public
+        returns (bool success)
+    {
+        allowance[msg.sender][_spender] = _value;
+        emit Approval(msg.sender, _spender, _value);
+        return true;
+    }
+}
