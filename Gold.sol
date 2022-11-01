7d8db7b2fa27c40bf4d5f9743f13d547b4e03513
Fernando H <fer.hndez@gmail.com>
2022-11-01 13:21:39 -0300

    contrato erc20

--- Gold.sol
+++ Gold.sol
@@ -12,36 +12,57 @@ contract Gold {
     mapping(address => uint256) public balanceOf;
     mapping(address => mapping(address => uint256)) public allowance;
 
+    constructor() public {
+        name = "Gold";
+        symbol = "GLD";
+        decimals = 18;
+        totalSupply = 1000 * (uint256(10)**decimals);
+        balanceOf[msg.sender] = totalSupply;
+    }
+
     //eventos
     event Transfer(address indexed _from, address indexed _to, uint256 _value);
-    event Approval(address indexed _owner, address indexed _spender, uint256 _value);
+    event Approval(
+        address indexed _owner,
+        address indexed _spender,
+        uint256 _value
+    );
 
     //funciones setter
 
     //transfer, primero require de que el balance del sender sea mayor o igual al valor que esta enviando
     //Compensar el valor de transferencia
-    function transfer(address _to, uint256 _value) public returns (bool success){
-        require(balanceOf[msg.sender]>= _value);
+    function transfer(address _to, uint256 _value)
+        public
+        returns (bool success)
+    {
+        require(balanceOf[msg.sender] >= _value);
         balanceOf[msg.sender] -= _value;
         balanceOf[_to] += _value;
-        emit Transfer(msg.sender,_to,_value);
+        emit Transfer(msg.sender, _to, _value);
         return true;
     }
 
-
-    function transferFrom(address _from, address _to, uint256 _value) public returns (bool success){
-
+    function transferFrom(
+        address _from,
+        address _to,
+        uint256 _value
+    ) public returns (bool success) {
+        require(balanceOf[_from] >= _value);
+        require(allowance[_from][msg.sender] >= _value);
+        balanceOf[_from] -= _value;
+        balanceOf[_to] += _value;
+        allowance[_from][msg.sender] -= _value;
+        emit Transfer(_from, _to, _value);
+        return true;
     }
-    function approve(address _spender, uint256 _value) public returns (bool success){
-        
+
+    function approve(address _spender, uint256 _value)
+        public
+        returns (bool success)
+    {
+        allowance[msg.sender][_spender] = _value;
+        emit Approval(msg.sender, _spender, _value);
+        return true;
     }
-    function allowance(address _owner, address _spender) public view returns (uint256 remaining){
-
-    }
-
-
-
-
-
-
 }
